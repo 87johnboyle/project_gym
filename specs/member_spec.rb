fd27a3cbe25e85aa41fe9ecd0259ac29439d390a
87johnboyle <john.hazukisan@gmail.com>
2020-02-02 19:32:28 +0000

    specs

---
+++ specs/member_spec.rb
@@ -0,0 +1,29 @@
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative ('../models/member')
+
+class TestMember < MiniTest::Test
+
+  def setup
+    @member1 = Member.new({
+      'first_name' => 'John',
+      'last_name' => 'Boyle',
+      'premium' => TRUE
+      })
+    end
+
+  def test_member_first_name
+    assert_equal("John", @member1.first_name)
+  end
+
+  def test_member_last_name
+    assert_equal("Boyle", @member1.last_name)
+  end
+
+  def test_member_premium
+    assert_equal(TRUE, @member1.premium)
+  end
+
+
+
+end
---
+++ specs/session_spec.rb
@@ -0,0 +1,27 @@
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative ('../models/gym_class')
+
+class TestSession < MiniTest::Test
+
+  def setup
+    @session1 = Session.new({
+      'name' => 'Boxing',
+      'genre' => 'Combat',
+      'capacity' => 20
+      })
+    end
+
+  def test_class_name
+    assert_equal("Boxing", @session1.name)
+  end
+
+  def test_class_genre
+    assert_equal("Combat", @session1.genre)
+  end
+
+  def test_class_capacity
+    assert_equal(20, @session1.capacity)
+  end
+
+end
