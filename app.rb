86284373fbf5cb85e81cadc5a51f8e6c286ffdb1
87johnboyle <john.hazukisan@gmail.com>
2020-02-04 09:12:55 +0000

    Search removed

--- app.rb
+++ app.rb
@@ -7,8 +7,3 @@ require_relative('controllers/members_controller')
 get '/' do
   erb ( :home )
 end
-
-get '/search/:name' do
-  @results = Session.all.where("name LIKE ?", "%#{params[:name]}%").reverse_order(:id)
-  erb ( :search )
-end
